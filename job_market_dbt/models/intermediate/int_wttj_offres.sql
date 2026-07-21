0acfbd49bfa75330d2e5d85c479637283a073bec
FALLON Clemence <Clemence.FALLON@berger-levrault.com>
2026-07-21 19:39:20 +0200

    style: simplification alias niveau_experience

--- job_market_dbt/models/intermediate/int_wttj_offres.sql
+++ job_market_dbt/models/intermediate/int_wttj_offres.sql
@@ -33,7 +33,7 @@ with wttj_intermediate as (
             when niveau_formation is null then 'non renseigné'
         end as niveau_formation,
 
-        niveau_experience as niveau_experience,
+        niveau_experience,
 
         trim(json_value(offices, '$[0].city')) as ville,
         trim(json_value(offices, '$[0].district')) as departement,
